0775ec6709d6d3e20974bd3cc2bdc2c3d49277c5
Adem Mrizak <adem.mrizak@gmail.com>
2022-11-21 18:40:39 +0100

    Fuji deploy conf

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+    solidity: "0.8.17",
+    networks: {
+        fuji: {
+            url: "https://api.avax-test.network/ext/bc/C/rpc",
+            gasPrice: "auto",
+            chainId: 43113,
+            accounts: ["YOURPRIVATEACOUNTKEY"]
+        }
+    }
 };
